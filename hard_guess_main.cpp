af6f0f99bc3c33b83adbafa43ca198040dec70fe
tomoshika256 <93367508+tomoshika256@users.noreply.github.com>
2024-01-16 17:03:25 +0900

    Update hard_guess_main.cpp

--- hard_guess_main.cpp
+++ hard_guess_main.cpp
@@ -1,5 +1,7 @@
 # include <Siv3D.hpp>
-void Post(int guess_num,int seed_num) {
+#include <iostream>
+#include <string>
+void Post(int guess_num, int seed_num) {
 	const URL url = U"http://tk2-242-30965.vs.sakura.ne.jp:8080/03/guessNumberHard/";
 	const HashTable<String, String> headers = { { U"Content-Type", U"application/json" } };
 	const std::string data = JSON{
@@ -17,7 +19,7 @@ void Post(int guess_num,int seed_num) {
 		Console << U"------";
 		Console << response.getHeader().rtrimmed();
 		Console << U"------";
-		
+
 		if (response.isOK())
 		{
 			Print << TextReader{ saveFilePath }.readAll();
@@ -31,13 +33,28 @@ void Post(int guess_num,int seed_num) {
 
 void Main()
 {
-	
+	int guess_num=-1;
+	int seed_num;
+	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+	TextEditState te;
+	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
+
 	while (System::Update())
 	{
+		
+		SimpleGUI::TextBox(te, Vec2{ 100, 140 });
+		font(U"設定値は{}"_fmt(guess_num)).draw(64, Vec2{ 20, 20 }, ColorF{ 0.2, 0.4, 0.8 });
+		if (te.text.isEmpty()==0&&KeyEnter.down())
+			guess_num = Parse<int>(te.text);
+		/*
+		* seed_num = 1;
 		if (KeyA.down())
-			Post(5,5);
+			Post(guess_num, seed_num);
+		*/
 		
 	}
+
+
+
+
 }
-
-
